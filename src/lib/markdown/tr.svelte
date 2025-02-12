cc9cf5e21fcf2cfe5b89bf53986c5740903a94ff
Cfp <chaosflo44plus@gmail.com>
2025-02-12 17:40:11 +0100

    💄 Change even table background color

--- src/lib/markdown/tr.svelte
+++ src/lib/markdown/tr.svelte
@@ -5,6 +5,6 @@
 	export { className as class };
 </script>
 
-<tr class={cn("even:bg-muted m-0 border-t p-0", className)} {...$$restProps}>
+<tr class={cn("even:bg-[hsl(222.2,84%,9.2%)] m-0 border-t p-0", className)} {...$$restProps}>
 	<slot />
 </tr>
